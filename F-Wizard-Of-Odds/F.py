5a6000596169b66aa4788a56b1079d685a2930ba
MattR2718 <matthew.ranson2718@gmail.com>
2024-09-27 12:29:24 +0100

    F

--- F-Wizard-Of-Odds/F.py
+++ F-Wizard-Of-Odds/F.py
@@ -1,2 +1,12 @@
-def ps(n):
-    prime = []
+import math
+inp = input().split(" ")
+n = int(inp[0])
+k = int(inp[1])
+
+found = math.log(n, 2)
+
+
+if(found <= k):
+    print("Your wish is granted!")
+else:
+    print("You will become a flying monkey!")
